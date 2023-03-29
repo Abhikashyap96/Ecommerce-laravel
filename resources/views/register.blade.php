49da18a81af33505a570319590466cc8480f3def
Abhikashyap96 <abhishekk62861@gmail.com>
2023-03-29 21:26:30 +0530

    Register done

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,9 +4,13 @@
 <div class="container custom-login">
     <div class="row">
         <div class="col-sm-4 col-sm-offset-4">
-        <form action="login" method="POST">
+        <form action="register" method="POST">
+        @csrf
   <div class="form-group">
-    @csrf
+    <label for="exampleInputEmail1">User Name</label>
+    <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter User Name">
+  </div>
+  <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
     <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
     
@@ -16,8 +20,8 @@
     <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
   </div>
   
-  <button type="submit" class="btn btn-primary">Login</button>
-  <p>Not registered then <a href="register">register</a> first</p>
+  <button type="submit" class="btn btn-primary">Register</button>
+  <p>already registered then <a href="/login">login</a></p>
 </form>
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,9 @@ Route::get('/logout',function(){
     Session::forget('user');
     return view('login');
 });
+Route::view("register",'register');
 Route::post("/login",[UserController::class,'login']);
+Route::post("/register",[UserController::class,'register']);
 Route::get("/",[ProductController::class,'index']);
 Route::get("detail/{id}",[ProductController::class,'detail']);
 Route::get("search",[ProductController::class,'search']);
@@ -35,4 +37,5 @@ Route::get("removecart/{id}",[ProductController::class,'removeCart']);
 Route::get("ordernow",[ProductController::class,'orderNow']);
 Route::post("orderplace",[ProductController::class,'orderPlace']);
 Route::get("myorders",[ProductController::class,'myOrders']);
+
 
