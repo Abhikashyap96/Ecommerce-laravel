9f1eaba0c6403627daaa62725649de9986ca9043
Abhishek kumar <abhishekk62861@gmail.com>
2023-12-15 13:01:00 +0530

    change content for header

--- resources/views/cartlist.blade.php
+++ resources/views/cartlist.blade.php
@@ -3,33 +3,33 @@
 
 <div class="custom-product">
   <div class="col-sm-10">
-  <div class="trending-wrapper">
-    <h4> Result for Products</h4>
-    <a class="btn btn-success" href="ordernow">Order Now</a>
-    @foreach ($products as $item)
-    <div class="row searched-item cart-list-devider">
-     <div class="col-sm-3">
-     <a href="detail/{{$item->id}}">
-      <img class="trending-img" src="{{$item->gallery}}">
-     
-     </a>
-     </div>
-     <div class="col-sm-4">
-     <a href="detail/{{$item->id}}">
-      
-      <div class="">
-        <h3>{{$item->name}}</h3>
-        <h5>{{$item->discription}}</h5>
+    <div class="trending-wrapper">
+      <h4> Result for Cart-Products</h4>
+      <a class="btn btn-success" href="ordernow">Order Now</a>
+      @foreach ($products as $item)
+      <div class="row searched-item cart-list-devider">
+        <div class="col-sm-3">
+          <a href="detail/{{$item->id}}">
+            <img class="trending-img" src="{{$item->gallery}}">
+
+          </a>
+        </div>
+        <div class="col-sm-4">
+          <a href="detail/{{$item->id}}">
+
+            <div class="">
+              <h3>{{$item->name}}</h3>
+              <h5>{{$item->discription}}</h5>
+            </div>
+          </a>
+        </div>
+        <div class="col-sm-3">
+          <a href="/removecart/{{$item->cart_id}}" class="btn btn-warning">Remove from cart</a>
+        </div>
       </div>
-     </a>
-     </div>
-     <div class="col-sm-3">
-     <a href="/removecart/{{$item->cart_id}}" class="btn btn-warning">Remove from cart</a>
-     </div>
+      @endforeach
+      <a class="btn btn-success" href="ordernow">Order Now</a>
     </div>
-    @endforeach
-    <a class="btn btn-success" href="ordernow">Order Now</a>
-  </div>
   </div>
 </div>
 @endsection
